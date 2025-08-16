e95f384817ca9b3d2674689c2afa60e6212ca9eb
Mark Kim <mark-kim@mail.ru>
2025-08-16 20:07:53 +0300

    finish

---
+++ go.mod
@@ -0,0 +1,3 @@
+module seabattle2
+
+go 1.24.5
---
+++ terminal/terminal.go
@@ -0,0 +1,13 @@
+package terminal
+
+import (
+	"os"
+	"os/exec"
+)
+
+func ClearTerminal() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
+}
